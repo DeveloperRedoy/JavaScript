4f718f12d02a68fb183a906288c8b26ba2e00af0
MD.Redoy Sarder <mdresponsivejb@gmail.com>
2022-01-12 22:54:20 +0600

    << concat() >> function used

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
-const str = "Hello World !";
+const str1 = "Hello World !";
 
-const ami = str.codePointAt();
+const str2 = "My name is MD.Redoy Sarder.";
 
+const ami = str1.concat(str2);
+// str1 and str2 string added
 document.write(ami);
