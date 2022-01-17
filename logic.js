a4e0859fb7ca0386fe733b2a0deed693515b01a2
MD.Redoy Sarder <mdresponsivejb@gmail.com>
2022-01-17 17:00:43 +0600

    OWN Web Compiler font-style .html .css .js

--- logic.js
+++ logic.js
@@ -1,11 +1,28 @@
-function showPreview(){
-    var htmlCode = document.getElementById("htmlCode").value;
-    var cssCode = "<style>"+document.getElementById("cssCode").value+"</style>";
-    var jsCode = "<scri"+"pt>"+document.getElementById("jsCode").value+"</scri"+"pt>";
+const htmlEditor = CodeMirror(document.querySelector(".editor .code .html-code"),{
+    lineNumbers:true,
+    tabSize:4,
+    mode:"xml"
+});
 
-    var frame = document.getElementById("preview-window").contentWindow.document;
+const cssEditor = CodeMirror(document.querySelector(".editor .code .html-code"),{
+    lineNumbers:true,
+    tabSize:4,
+    mode:"css"
+});
 
-    frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
-    frame.close();
-}
+const jsEditor = CodeMirror(document.querySelector(".editor .code .html-code"),{
+    lineNumbers:true,
+    tabSize:4,
+    mode:"javascript"
+});
+
+document.querySelector("#run-btn").addEventListener("click", function(){
+    let htmlCode = htmlEditor.getValue();
+    let cssCode = "<style>"+ cssEditor.getValue()+"</style>";
+    let jsCode = "<scri"+"pt>"+jsEditor.getValue()+"</scri"+"pt>";
+
+    let previewWindow = document.querySelector("#preview-window").contentWindow.document;
+    previewWindow.open();
+    previewWindow.write(htmlCode+cssCode+jsCode);
+    previewWindow.close();
+})
