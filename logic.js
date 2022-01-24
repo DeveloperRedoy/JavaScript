3b17754baa1bbe04297d206b016148da0a629e1e
MD.Redoy Sarder <mdresponsivejb@gmail.com>
2022-01-24 12:37:12 +0600

    Photo editor simple app create

--- logic.js
+++ logic.js
@@ -1,28 +1,15 @@
-const htmlEditor = CodeMirror(document.querySelector(".editor .code .html-code"),{
-    lineNumbers:true,
-    tabSize:4,
-    mode:"xml"
+$(document).ready(function()
+{
+ $(".range").change(add_filter).mousemove(add_filter);
 });
+	
+function add_filter()
+{
+ var grayscale_val=$("#grayscale").val();
+ var blur_val=$("#blur").val();
+ var exposure_val=$("#exposure").val();
+ var sepia_val=$("#sepia").val();
+ var opacity_val=$("#opacity").val();
 
-const cssEditor = CodeMirror(document.querySelector(".editor .code .html-code"),{
-    lineNumbers:true,
-    tabSize:4,
-    mode:"css"
-});
-
-const jsEditor = CodeMirror(document.querySelector(".editor .code .html-code"),{
-    lineNumbers:true,
-    tabSize:4,
-    mode:"javascript"
-});
-
-document.querySelector("#run-btn").addEventListener("click", function(){
-    let htmlCode = htmlEditor.getValue();
-    let cssCode = "<style>"+ cssEditor.getValue()+"</style>";
-    let jsCode = "<scri"+"pt>"+jsEditor.getValue()+"</scri"+"pt>";
-
-    let previewWindow = document.querySelector("#preview-window").contentWindow.document;
-    previewWindow.open();
-    previewWindow.write(htmlCode+cssCode+jsCode);
-    previewWindow.close();
-})
+ $("#edit_image").css("-webkit-filter","grayscale("+grayscale_val+"%) blur("+blur_val+"px) brightness("+exposure_val+"%) sepia("+sepia_val+"%) opacity("+opacity_val+"%)");
+}
